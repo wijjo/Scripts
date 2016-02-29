a6987ccb25099b7bc9f2d2072088354da2355117
Steve Cooper <github@wijjo.com>
2016-02-28 19:58:22 -0800

    submodule

--- lib/import_python_misc.py
+++ lib/import_python_misc.py
@@ -1,15 +1,18 @@
 import sys
 import os
 
-# Look for python_misc under a parent folder.
-folder = os.path.dirname(os.path.dirname(__file__))
-while not os.path.exists(os.path.join(folder, 'python_misc', 'python_misc', '__init__.py')):
-    folder = os.path.dirname(folder)
-    if folder == '/':
-        sys.stderr.write('''
+# Look for python_misc under ext or the parent folder.
+python_misc = None
+repo_root = os.path.dirname(os.path.dirname(__file__))
+for base in (os.path.join(repo_root, 'ext'), os.path.dirname(repo_root)):
+    python_misc_chk = os.path.join(base, 'python_misc')
+    if os.path.exists(os.path.join(python_misc_chk, 'python_misc', '__init__.py')):
+        python_misc = python_misc_chk
+        break
+else:
+    sys.stderr.write('''
 python_misc library not found
 ''')
-        sys.exit(1)
-path = os.path.join(folder, 'python_misc')
-if not path in sys.path:
-    sys.path.insert(0, path)
+    sys.exit(1)
+if python_misc not in sys.path:
+    sys.path.insert(0, python_misc)
