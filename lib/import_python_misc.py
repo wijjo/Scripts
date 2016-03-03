36aaa7791272b1697cc03664f222bfe0c386187e
Steve Cooper <github@wijjo.com>
2016-03-03 23:48:51 +0000

    Freeze self-contained executables

--- lib/import_python_misc.py
+++ lib/import_python_misc.py
@@ -8,11 +8,6 @@ for base in (os.path.join(repo_root, 'ext'), os.path.dirname(repo_root)):
     python_misc_chk = os.path.join(base, 'python_misc')
     if os.path.exists(os.path.join(python_misc_chk, 'python_misc', '__init__.py')):
         python_misc = python_misc_chk
+        if python_misc not in sys.path:
+            sys.path.insert(0, python_misc)
         break
-else:
-    sys.stderr.write('''
-python_misc library not found
-''')
-    sys.exit(1)
-if python_misc not in sys.path:
-    sys.path.insert(0, python_misc)
