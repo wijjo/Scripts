5f30ab16ad4db44a28d65d8dab5d7840574871ee
Steve Cooper <github@wijjo.com>
2016-07-17 03:25:54 +0000

    rename python_misc repo to scriptbase

--- bin/lib/import_scriptbase.py
+++ bin/lib/import_scriptbase.py
@@ -1,17 +1,17 @@
 import sys
 import os
 
-# Where to look for python_misc/__init__.py under parent directories.
-BASE_SUB_DIRS = ('lib', os.path.join('ext', 'python_misc'))
+# Where to look for scriptbase/__init__.py under parent directories.
+BASE_SUB_DIRS = ('lib', os.path.join('ext', 'scriptbase'))
 
-def add_python_misc_to_sys_path():
-    # Look for lib/python_misc or ext/python_misc/python_misc in the directory hierarchy.
+def add_scriptbase_to_sys_path():
+    # Look for lib/scriptbase or ext/scriptbase/scriptbase in the directory hierarchy.
     last_base_dir = None
     base_dir = os.path.dirname(os.path.dirname(__file__))
     while True:
         for base_sub_dir in BASE_SUB_DIRS:
             base_lib_dir = os.path.join(base_dir, base_sub_dir)
-            if os.path.exists(os.path.join(base_lib_dir, 'python_misc', '__init__.py')):
+            if os.path.exists(os.path.join(base_lib_dir, 'scriptbase', '__init__.py')):
                 if base_lib_dir not in sys.path:
                     sys.path.insert(0, base_lib_dir)
                 return
@@ -23,6 +23,6 @@ def add_python_misc_to_sys_path():
 
 try:
     # Do nothing if it imports using the existing system path.
-    import python_misc.utility
+    import scriptbase.utility
 except ImportError:
-    add_python_misc_to_sys_path()
+    add_scriptbase_to_sys_path()
